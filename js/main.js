f7d0daf5dbd02450432051def3cd8a04650d3d59
Vladislav <vadimromashko@gmail.com>
2021-11-07 13:13:35 +0200

    added new blocks and styles

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
 $(document).ready(function () {
     $('#products-items').slick({
         dots: true,
-        dotsClass: 'slick-dots'
+        dotsClass: 'product-dots',
     });
-$('.slick-next.slick-arrow').text('')
-$('.slick-prev.slick-arrow').text('')
+    $('.slick-next.slick-arrow').text('')
+    $('.slick-prev.slick-arrow').text('')
 
 
     $('#recipe-items').slick({
         dots: true,
-        dotsClass: 'slick-dots'
+        dotsClass: 'recipe-dots',
+        arrows: false,
     });
+
+    $('#arr-next').click(function (e) {
+        $('#recipe-items').slick('slickNext');
+
+    });
+
+    $('#arr-prev').click(function (e) {
+        $('#recipe-items').slick('slickPrev');
+
+    });
+
+    new WOW().init();
+
 });
